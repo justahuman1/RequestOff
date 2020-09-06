d79f0bb93729d509f670c34b24d0e34b6fb5ad16
justahuman1 <saivdev@protonmail.com>
2020-09-06 00:25:25 -0700

    UI Render Background Data [Stable]

--- data/static/js/fillPopup.js
+++ data/static/js/fillPopup.js
@@ -1,9 +1,15 @@
-function generateSlider(i, offline) {
-  return `<label class="switch" for="checkbox${i}">
-    <input ${offline} type="checkbox" id="checkbox${i}"/>
+function generateSlider(tabId, offline) {
+  return `<label class="switch" for="${tabId}">
+    <input ${offline}  type="checkbox" id="${tabId}"/>
     <div class="slider round"></div></label>`;
 }
+function updateBackendOfflineMap(data) {
+  console.log(data);
+}
 async function traverseTabs(tabs) {
+  // Communicator between background and UI
+  // Get current offline tabs, populate accordingly,
+  // allow for updates, and send updates back to background script
   const offlineTabs = await browser.runtime.sendMessage({
     type: "getOfflineTabs",
   });
@@ -13,14 +19,20 @@ async function traverseTabs(tabs) {
     let row = table.insertRow(++i);
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
-    if (offlineTabs.has(tab.id)) cell1.innerHTML = generateSlider(i, "checked");
-    else cell1.innerHTML = generateSlider(i);
+    if (offlineTabs.has(tab.id))
+      cell1.innerHTML = generateSlider(tab.id, "checked");
+    else cell1.innerHTML = generateSlider(tab.id);
 
     cell2.innerHTML = tab.title;
+    cell1.addEventListener("click", (event) => {
+      let uiClickedId = event.target.getAttribute("id");
+      if (uiClickedId) {
+      }
+    });
   }
 }
-
 function fillOnlineTab() {
+  // Get tabs and populate UI
   browser.tabs
     .query({ currentWindow: true })
     .then(traverseTabs)
