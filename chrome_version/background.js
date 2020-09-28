1dd8cd22d6190b6da51a8fdeae2c9aae7cbb655a
justahuman1 <saivdev@protonmail.com>
2020-09-27 18:29:07 -0700

    Added Chrome Version

---
+++ chrome_version/background.js
@@ -0,0 +1,104 @@
+chrome.contextMenus.create(
+  // Used for context menu (quick toggle)
+  {
+    id: "offline-tab",
+    title: "Toggle Tab Requests",
+    contexts: ["all"],
+  }, // Initalize RO storage for serialized disk buffer
+  () => localStorage.setItem("offline_tabs_ro", "")
+);
+// Set used of O(1) operations (goal: minimize injected script overhead)
+const inMemoryStorage = new Set();
+
+function sendOfflineMessage(tabId, tabTitle = "") {
+  // Trigger to add tab to inMemoryStorage if not
+  // in set. Else, remove. Provides toggle functionality.
+  let dynamicNotiDetails = {};
+  if (inMemoryStorage.has(tabId)) {
+    dynamicNotiDetails["message"] = "This tab is now live!";
+    dynamicNotiDetails["iconUrl"] = "data/svg/online.svg";
+    inMemoryStorage.delete(tabId);
+  } else {
+    // Tab not found in store. Add and Send corresponding noti
+    dynamicNotiDetails[
+      "message"
+    ] = `This tab is now offline: "${tabTitle.substring(0, 15)}..."`;
+    dynamicNotiDetails["iconUrl"] = "data/svg/offline.svg";
+    inMemoryStorage.add(tabId);
+  }
+  // Add to block list and send notification
+  if (tabTitle) {
+    chrome.notifications.create({
+      type: "basic",
+      title: "RequestOff",
+      ...dynamicNotiDetails,
+    });
+  }
+}
+// // Communicator with popup html
+chrome.runtime.onMessage.addListener((data) => {
+  switch (data.type) {
+    case "getOfflineTabs":
+      console.log("NMOOOOO");
+      console.log();
+      // Send store to frontend
+      return JSON.stringify([...inMemoryStorage]);
+    // return Promise.resolve(inMemoryStorage);
+    case "addOfflineTab":
+      // Get UI change and update store
+      sendOfflineMessage(Number(data.id));
+      return Promise.resolve(null);
+    default:
+      return;
+  }
+});
+// // Shortcut commands
+// browser.commands.onCommand.addListener((command) => {
+//   // Dispatch offline mode on current active tab
+//   if (command === "toggle-requests-in-tab") {
+//     browser.tabs.query({ currentWindow: true, active: true }).then(
+//       (res) => sendOfflineMessage(res[0].id, res[0].title),
+//       () => console.log("Shortcut Instantiation Failed")
+//     );
+//   }
+// });
+function normalizeGetLocalStorage() {
+  // Rather than pushing to localStorage (expensive), we will
+  // push the data to localStorage only when browser is closed
+  // via onClose trigger. Else, we use a set for quick ops.
+  // Old localStorage code in commit 61143d4
+  return inMemoryStorage;
+}
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+  if (info.menuItemId == "offline-tab") {
+    sendOfflineMessage(Number(tab.id), tab.title);
+    // Inject content script to monitor and prompt for going online
+    // browser.tabs.executeScript({
+    //   code: `alert("location:${}");`,
+    // });
+  }
+});
+// function injectConfirmGoingOnline() {}
+function cancelRequest(requestDetails) {
+  // Block request if tab in block-list and prompt going online
+  // if website is already offline and a request was detected
+  // TODO: Issue - Determine if request was user or server
+  // Idea: Determine if a click occured in the tab in the previous
+  // ~1 second before request occurs. If so, we can guess it was
+  // the user, rather than the server.
+  if (inMemoryStorage.has(requestDetails.tabId)) {
+    // injectConfirmGoingOnline();
+    // Chrome takes a different approach to
+    // requestBlocking than Firefox
+    return { redirectUrl: "javascript:" };
+  }
+}
+chrome.webRequest.onBeforeRequest.addListener(
+  // Main event dispatcher for request management
+  // Fires on all requests (as the extension may run
+  // on all URL's). Blocking mode enabled to compltely
+  // reject incoming & outgoing requests.
+  cancelRequest,
+  { urls: ["<all_urls>"] },
+  ["blocking"]
+);
---
+++ chrome_version/data/static/js/fillPopup.js
@@ -0,0 +1,44 @@
+function generateSlider(tabId, offline) {
+  // Dynamic Slider element
+  return `<label class="switch" for="${tabId}">
+    <input ${offline}  type="checkbox" id="${tabId}"/>
+    <div class="slider round"></div></label>`;
+}
+async function traverseTabs(tabs) {
+  // Communicator between background and UI
+  // Get current offline tabs, populate accordingly,
+  // allow for updates, and send updates back to background script
+  const offlineTabs = await chrome.runtime.sendMessage({
+    type: "getOfflineTabs",
+  });
+  console.log("Poop");
+  console.log(offlineTabs);
+  // Get bare-bones table from popup.html
+  const table = document.getElementById("mainTable");
+  let i = 0;
+  for (let tab of tabs) {
+    let row = table.insertRow(++i);
+    let cells = [row.insertCell(0), row.insertCell(1)];
+    // Check if tab is in store (background message)
+    if (offlineTabs.has(tab.id))
+      cells[0].innerHTML = generateSlider(tab.id, "checked");
+    else cells[0].innerHTML = generateSlider(tab.id);
+    // Title cell (same amount of chars as default tab length)
+    cells[1].innerHTML = tab.title.substring(0, 20);
+    // Add UI listener per event for corresponding tab (via global ID)
+    cells[0].addEventListener("click", (event) => {
+      let uiClickedId = event.target.getAttribute("id");
+      if (uiClickedId) {
+        browser.runtime.sendMessage({ type: "addOfflineTab", id: uiClickedId });
+      }
+    });
+  }
+}
+function fillOnlineTab() {
+  // Get tabs and populate UI on popup click
+  chrome.tabs.query({ currentWindow: true }, traverseTabs);
+  // .then()
+  // .catch(() => alert("Failed Window Parsing.."));
+}
+// Initalizer function for UI
+fillOnlineTab();
